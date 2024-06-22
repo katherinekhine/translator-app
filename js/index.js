95181c279bfb9734ad5583ea19d672bd8b0731de
katherinekhine <khaingkatherine@gmail.com>
2024-06-22 19:13:47 +0630

    update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
-const selectTag = document.querySelectorAll("select");
+const fromText = document.querySelector(".from-text"),
+  toText = document.querySelector(".to-text"),
+  selectTag = document.querySelectorAll("select"),
+  exchangeIcon = document.querySelector(".exchange"),
+  translateBtn = document.querySelector("button");
 
 selectTag.forEach((tag, id) => {
   for (const country_code in countries) {
@@ -12,4 +16,27 @@ selectTag.forEach((tag, id) => {
     let option = ` <option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
     tag.insertAdjacentHTML("beforeend", option);
   }
+});
+
+exchangeIcon.addEventListener("click", () => {
+  // exchanging textarea and select tag values
+  let tempText = fromText.value;
+  tempLang = selectTag[0].value;
+  fromText.value = toText.value;
+  selectTag[0].value = selectTag[1].value;
+  toText.value = tempText;
+  selectTag[1].value = tempLang;
+});
+
+translateBtn.addEventListener("click", () => {
+  let text = fromText.value,
+    translateFrom = selectTag[0].value, //getting fromSelect tag value
+    translateTo = selectTag[1].value; //getting toSelect tag value
+  let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+  fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      toText.value = data.responseData.translatedText;
+    });
 });
